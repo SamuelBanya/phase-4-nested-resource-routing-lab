f562faacfcc7059cc9ca0923a7799e3cba890a67
Samuel Banya <sbanya@fastmail.com>
2022-10-26 20:28:58 -0500

    Adding latest changes to assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
-  resources :users, only: [:show]
+
+  # This allows someone to access the 'users' resource
+  # and as a result, they can also access '/items'
+  # aftewrards, but only to use the ':index',
+  # ':show', and ':create' actions so that they
+  # can create individual 'items' for 'users':
+  resources :users, only: [:show] do
+    resources :items, only: [:index, :show, :create]
+  end
 end
